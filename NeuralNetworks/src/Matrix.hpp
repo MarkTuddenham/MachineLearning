d98336966445b23d6abcc58aa2865932c1b7448d
Mark Tuddenham <mark@tudders.com>
2019-05-13 13:10:12 +0100

    Format correctly and use const Matrix & where possible

--- NeuralNetworks/src/Matrix.hpp
+++ NeuralNetworks/src/Matrix.hpp
@@ -6,38 +6,40 @@ class Matrix;
 #include <iostream>
 #include <functional>
 
+class Matrix
+{
+private:
+  unsigned int rows, cols;
+  std::vector<std::vector<double>> d;
+  std::string name;
 
-class Matrix {
-  private:
-      int rows, cols;
-      std::vector< std::vector<double> > d;
-      std::string name;
+  Matrix elementwise(const Matrix &m, std::function<double(double, double)> f) const;
 
-      Matrix elementwise(Matrix m, std::function<double(double, double)> f) const;
+public:
+  Matrix(int rows, int cols, double fill = 0, std::string name = "M");
 
-  public:
+  Matrix randomise(void);
 
-    Matrix(int rows, int cols, double fill = 0, std::string name = "M");
+  Matrix multiply(const Matrix &) const;
+  Matrix operator*(const Matrix &)const;
+  Matrix transpose(void) const;
+  Matrix operator~() const;
+  Matrix add(const Matrix &) const;
+  Matrix operator+(const Matrix &) const;
+  Matrix subtract(const Matrix &) const;
+  Matrix operator-(const Matrix &) const;
 
-    Matrix randomise(void);
+  Matrix hadamard(const Matrix &) const;
+  // Matrix operator->*(const Matrix&) const;
 
-    Matrix multiply(Matrix) const;
-    Matrix operator*(const Matrix&) const;
-    Matrix transpose(void) const;
-    Matrix operator~() const;
-    Matrix add(Matrix) const;
-    Matrix operator+(const Matrix&) const;
-    Matrix subtract(Matrix) const;
-    Matrix operator-(const Matrix&) const;
-    Matrix hadamard(Matrix) const;
-    Matrix operator->*(const Matrix&) const;
+  Matrix apply(std::function<double(double)> f) const;
 
-    Matrix apply(std::function<double(double)> f) const;
+  std::vector<double> flatten(void) const;
 
-    static Matrix fromArray(std::vector<double>);
-    std::vector<double> flatten(void) const;
-    void print(int precision = 3,std::ostream* op = &std::cout) const;
-    int getRows() const;
-    int getCols() const;
-    void setName(std::string name);
+  void print(int precision = 3, std::ostream *op = &std::cout) const;
+  unsigned int getRows() const;
+  unsigned int getCols() const;
+  void setName(std::string name);
+
+  static Matrix fromArray(const std::vector<double> &);
 };
