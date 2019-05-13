fbb2343107ed57c8e5ab67a859ae56593e2ab59b
Mark Tuddenham <mark@tudders.com>
2019-05-13 13:08:48 +0100

    Neural Networks now use Matrices as inputs

--- NeuralNetworks/src/NeuralNetwork.cpp
+++ NeuralNetworks/src/NeuralNetwork.cpp
@@ -4,89 +4,99 @@
 #include <functional>
 #include <cmath>
 
-NeuralNetwork::NeuralNetwork(std::vector<int> layers): layers(layers) {
+NeuralNetwork::NeuralNetwork(std::vector<int> layers) : layers(layers)
+{
   // Compute initial weights & biases
   // inp layer + hidden layers + out layer
 
   //TODO check layers cout >=3 or 2?
-  for (size_t i = 1; i < layers.size(); i++) {
-    this->w.push_back(Matrix(layers[i], layers[i-1]).randomise());
-    this->b.push_back(Matrix(layers[i], 1          ).randomise());
+  for (unsigned int i = 1; i < layers.size(); i++)
+  {
+    this->w.push_back(Matrix(layers[i], layers[i - 1]).randomise());
+
+    // this->w.push_back(Matrix(layers[i], layers[i - 1]).randomise());
+    // this->b.push_back(Matrix(layers[i], 1).randomise());
   }
 };
 
-Matrix NeuralNetwork::feedforward(double* inputs) {
+Matrix NeuralNetwork::feedforward(const Matrix &inputs)
+{
   this->outs.clear();
   // turn inp array into matrix to calculate feedforward
   // call it out to use as first instance in the loop
-  std::vector<double> ip(inputs, inputs + layers.front());
-  Matrix out = ~Matrix::fromArray(ip);
+
+  Matrix out = inputs.transpose();
   out.setName("inp");
   this->outs.push_back(out);
 
-  for (size_t i = 0; i < layers.size()-1; i++) {
-    Matrix net = (w[i]*out) + b[i];
+  for (unsigned int i = 0; i < layers.size() - 1; i++)
+  {
+    Matrix net = (w[i] * out); // + b[i];
     out = NeuralNetwork::applyActivationFunction(net, NeuralNetwork::Sigmoid);
-    out.setName("out_"+std::to_string(i+1));
+    out.setName("out_" + std::to_string(i + 1));
     this->outs.push_back(out);
   }
   out.setName("Out");
   return out;
 }
 
-void NeuralNetwork::backpropagation(double* inputs, double* targets, double eta){
+void NeuralNetwork::backpropagation(const Matrix &inputs, const Matrix &targets, double eta = 0.1)
+{
   //compute total error
   feedforward(inputs);
-  Matrix t = Matrix::fromArray(std::vector<double>(targets, targets + layers.back())).transpose();
-
+  Matrix t = targets.transpose();
 
   // calculate weight errors
 
   //TODO only sigmoid atm
-  std::function<Matrix(Matrix)> delta_activ = [](Matrix m){return m->*(Matrix(m.getRows(),m.getCols(), 1) - m);};
+  std::function<Matrix(Matrix)> delta_activ = [](Matrix m) {
+    return m.hadamard(Matrix(m.getRows(), m.getCols(), 1) - m);
+  };
 
   std::vector<Matrix> w_e;
   //weight to output layers (H_N->O) are computed with a different node delta
-  int N = layers.size()-1;
-  Matrix node_delta = (outs[N]-t)->*delta_activ(outs[N]);
-  w_e.insert(w_e.begin(), Matrix(1,1,eta)->*node_delta*~outs[N-1]);
+  int N = layers.size() - 1;
+  Matrix node_delta = (outs[N] - t).hadamard(delta_activ(outs[N]));
+  w_e.insert(w_e.begin(), Matrix(1, 1, eta).hadamard(node_delta * ~outs[N - 1]));
 
-
-  // loop to compute node deltas and weight error responibilites from input to last hidden layer (I->H_[N-1])
-  for (size_t i = layers.size()-2; i > 0; i--) {
-    node_delta = ((~w[i])*node_delta) ->* delta_activ(outs[i]);
-    w_e.insert(w_e.begin(), Matrix(1,1,eta)->*node_delta*~outs[i-1]);
+  // loop to compute node deltas and weight error responsibilities from input to last hidden layer (I->H_[N-1])
+  for (unsigned int i = layers.size() - 2; i > 0; i--)
+  {
+    node_delta = ((~w[i]) * node_delta).hadamard(delta_activ(outs[i]));
+    w_e.insert(w_e.begin(), Matrix(1, 1, eta).hadamard(node_delta * ~outs[i - 1]));
   }
 
   //update weights
-  for (size_t i = 0; i < w.size(); i++) {
-    w[i] = w[i]-w_e[i];
+  for (unsigned int i = 0; i < w.size(); i++)
+  {
+    w[i] = w[i] - w_e[i];
   }
-
 }
 
-Matrix NeuralNetwork::applyActivationFunction(Matrix m, ActivationFunction af){
+Matrix NeuralNetwork::applyActivationFunction(const Matrix &m, ActivationFunction af)
+{
   std::function<double(double)> f;
 
-  switch (af) {
-    case Sigmoid:
-      f = [](double d){
-        return 1/(1+std::exp(-d));
-      };
-      break;
-    case FastSigmoid:
-      f = [](double d){
-        return d/(1+std::abs(d));
-      };
-      break;
-    case Tanh:
-      f = [](double d){
-        return std::tanh(d);
-      };
-      break;
-    default:
-      throw std::invalid_argument("Unknown Activation Function.");
-      break;
+  switch (af)
+  {
+  case Sigmoid:
+    f = [](double d) {
+      return 1 / (1 + std::exp(-d));
+    };
+    break;
+  case FastSigmoid:
+    f = [](double d) {
+      return d / (1 + std::abs(d));
+    };
+    break;
+  case Tanh:
+    f = [](double d) {
+      return std::tanh(d);
+    };
+    break;
+  default:
+    throw std::invalid_argument("Unknown Activation Function.");
+    break;
   }
   return m.apply(f);
 }
--- NeuralNetworks/src/NeuralNetwork.hpp
+++ NeuralNetworks/src/NeuralNetwork.hpp
@@ -1,21 +1,24 @@
 #pragma once
 
-class NerualNetwork;
-
 #include "Matrix.hpp"
 #include <vector>
 
-class NeuralNetwork {
-  private:
-      const std::vector<int> layers;
-      std::vector<Matrix> w,b,outs;
+class NeuralNetwork
+{
+private:
+  const std::vector<int> layers;
+  std::vector<Matrix> w, b, outs;
 
+public:
+  NeuralNetwork(std::vector<int>);
+  Matrix feedforward(const Matrix &);
+  void backpropagation(const Matrix &, const Matrix &, double);
 
-  public:
-    NeuralNetwork(std::vector<int>);
-    Matrix feedforward(double*);
-    void backpropagation(double*, double*, double eta = 0.1);
-
-    enum ActivationFunction {Sigmoid, FastSigmoid, Tanh};
-    static Matrix applyActivationFunction(Matrix m, ActivationFunction af);
+  enum ActivationFunction
+  {
+    Sigmoid,
+    FastSigmoid,
+    Tanh
+  };
+  static Matrix applyActivationFunction(const Matrix &, ActivationFunction);
 };
