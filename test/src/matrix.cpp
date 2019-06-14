b6d3a24a3660afa07db535fc4b40afd2d71d6f77
Mark Tuddenham <mark@tudders.com>
2019-06-14 23:47:58 +0100

    Add some matrix tests

--- test/src/matrix.cpp
+++ test/src/matrix.cpp
@@ -2,18 +2,55 @@
 #include <sstream>
 
 #include <matrix.hpp>
+#include <utils.hpp>
 
-namespace Catch
+TEST_CASE("Matrix Constructor", "[matrix]")
 {
+    unsigned int n_rows = 5, n_cols = 3;
 
-template <typename T>
-std::string toString(Matrix<T> const &m)
-{
-    std::stringstream ss;
-    m.print(3, ss);
-    return ss.str();
+    Matrix<double> m = Matrix<double>(n_rows, n_cols);
+    REQUIRE(m.getRows() == n_rows);
+    REQUIRE(m.getCols() == n_cols);
 }
-} // namespace Catch
+
+TEST_CASE("Matrix Zeros", "[matrix]")
+{
+    unsigned int n_rows = 5, n_cols = 3;
+
+    Matrix<double> m1 = Matrix<double>(n_rows, n_cols, 0);
+    Matrix<double> m2 = Matrix<double>::zeros(n_rows, n_cols);
+
+    REQUIRE(m1 == m2);
+}
+
+TEST_CASE("Matrix Ones", "[matrix]")
+{
+    unsigned int n_rows = 5, n_cols = 3;
+
+    Matrix<double> m1 = Matrix<double>(n_rows, n_cols, 1);
+    Matrix<double> m2 = Matrix<double>::ones(n_rows, n_cols);
+
+    REQUIRE(m1 == m2);
+}
+
+TEST_CASE("Matrix Transpose", "[matrix][!mayfail]")
+{
+    unsigned int n_rows = 5, n_cols = 3;
+    Matrix<double> m1 = Matrix<double>(n_rows, n_cols).randomise();
+    Matrix<double> m2 = Matrix<double>(n_cols, n_rows);
+
+    REQUIRE(m1.transpose() == m2);
+}
+
+// TEST_CASE("Matrix Apply", "[matrix][!mayfail]")
+// {
+//     //    TODO
+// }
+
+// TEST_CASE("Matrix Flatten", "[matrix][!mayfail]")
+// {
+//     //    TODO
+// }
 
 TEST_CASE("Matrix Equality", "[matrix]")
 {
@@ -22,10 +59,17 @@ TEST_CASE("Matrix Equality", "[matrix]")
     REQUIRE(m1 == m2);
 }
 
+TEST_CASE("Matrix Randomise", "[matrix]")
+{
+    Matrix<double> m1 = Matrix<double>(2, 2);
+    Matrix<double> m2 = m1;
+    REQUIRE_FALSE(m1.randomise() == m2);
+}
+
 TEST_CASE("Matrix Addition", "[matrix]")
 {
     double a = 4.2, b = 17.73;
-    uint n_rows = 5, n_cols = 3;
+    unsigned int n_rows = 5, n_cols = 3;
 
     Matrix<double> m_a = Matrix<double>(n_rows, n_cols, a);
     Matrix<double> m_b = Matrix<double>(n_rows, n_cols, b);
@@ -38,7 +82,7 @@ TEST_CASE("Matrix Addition", "[matrix]")
 
     SECTION(" by matrix")
     {
-        Matrix<double> m_c = Matrix<double>(n_rows, n_cols, a + b);
+        Matrix<double> m_c = Matrix<double>(n_rows, n_cols, a + b, "Expected");
         REQUIRE(m_a + m_b == m_c);
     }
 }
@@ -46,7 +90,7 @@ TEST_CASE("Matrix Addition", "[matrix]")
 TEST_CASE("Matrix Subtraction", "[matrix]")
 {
     double a = 4.2, b = 17.73;
-    uint n_rows = 5, n_cols = 3;
+    unsigned int n_rows = 5, n_cols = 3;
 
     Matrix<double> m_a = Matrix<double>(n_rows, n_cols, a);
     Matrix<double> m_b = Matrix<double>(n_rows, n_cols, b);
@@ -59,7 +103,7 @@ TEST_CASE("Matrix Subtraction", "[matrix]")
 
     SECTION(" by matrix")
     {
-        Matrix<double> m_c = Matrix<double>(n_rows, n_cols, a - b);
+        Matrix<double> m_c = Matrix<double>(n_rows, n_cols, a - b, "Expected");
         REQUIRE(m_a - m_b == m_c);
     }
 }
@@ -67,7 +111,7 @@ TEST_CASE("Matrix Subtraction", "[matrix]")
 TEST_CASE("Matrix Multiplication", "[matrix]")
 {
     double a = 4.2, b = 17.73;
-    uint n_a_rows = 5, n_a_cols = 3;
+    unsigned int n_a_rows = 2, n_a_cols = 7;
 
     Matrix<double> m_a = Matrix<double>(n_a_rows, n_a_cols, a);
     Matrix<double> m_b = Matrix<double>(n_a_cols, n_a_rows, b);
@@ -80,7 +124,19 @@ TEST_CASE("Matrix Multiplication", "[matrix]")
 
     SECTION(" by matrix")
     {
-        Matrix<double> m_c = Matrix<double>::fromArray({});
+        Matrix<double> m_c = Matrix<double>(n_a_rows, n_a_rows, a * b * n_a_cols, "Expected");
         REQUIRE(m_a * m_b == m_c);
     }
+}
+
+TEST_CASE("Matrix Hadamard", "[matrix]")
+{
+    double a = 4.2, b = 17.73;
+    unsigned int n_rows = 5, n_cols = 3;
+
+    Matrix<double> m_a = Matrix<double>(n_rows, n_cols, a);
+    Matrix<double> m_b = Matrix<double>(n_rows, n_cols, b);
+
+    Matrix<double> m_c = Matrix<double>(n_rows, n_cols, a * b, "Expected");
+    REQUIRE(m_a.hadamard(m_b) == m_c);
 }
