d2627ebfcb5ca062298c4aab979938e7202bf14c
Mark Tuddenham <mark@tudders.com>
2019-07-21 13:53:33 +0100

    Fix matrix prining & add nn printing

--- lib/src/utils/printer.hpp
+++ lib/src/utils/printer.hpp
@@ -7,5 +7,5 @@ using Teslyn::NeuralNetwork;
 namespace Teslyn::display
 {
 void print_matrix(const Matrix &m, int t_precision = 3, std::ostream *t_op = &std::cout);
-void print_nn(const NeuralNetwork &nn, int t_precision = 3, std::ostream *t_op = &std::cout);
+void print_nn(const NeuralNetwork &nn, std::ostream *t_op = &std::cout);
 } // namespace Teslyn::display
